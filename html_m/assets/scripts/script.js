a5c2b47fdb6a096b35f980ea04b172b12e5efb70
Danilo Mogin <danilo.mogin@gmail.com>
2016-04-30 23:51:03 +0200

    dodato svega samo nista nije uredjeno do kraja

--- html_m/assets/scripts/script.js
+++ html_m/assets/scripts/script.js
@@ -1,20 +1,20 @@
 /*----------  equal col
 ------------------------------------------------------------------------------*/
-equalheight = function(container){
+equalheight = function (container) {
 
     var currentTallest = 0,
         currentRowStart = 0,
         rowDivs = new Array(),
         $el,
         topPosition = 0;
-    $(container).each(function() {
+    $(container).each(function () {
 
         $el = $(this);
         $($el).height('auto')
         topPostion = $el.position().top;
 
         if (currentRowStart != topPostion) {
-            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
                 rowDivs[currentDiv].height(currentTallest);
             }
             rowDivs.length = 0; // empty the array
@@ -25,36 +25,15 @@ equalheight = function(container){
             rowDivs.push($el);
             currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
         }
-        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
             rowDivs[currentDiv].height(currentTallest);
         }
     });
 }
 
-$(window).load(function() {
-
-/*----------  equal height call
-------------------------------------------------------------------------------*/
-    equalheight('.pricing .pricing-col');
-
-/*----------  flex slider call
-------------------------------------------------------------------------------*/
-    $('.flexslider').flexslider({
-        animation: "slide",
-        slideshow: false
-    });
-});
-
-$(window).resize(function() {
-
-/*----------  equal height call
-------------------------------------------------------------------------------*/
-    equalheight('.pricing .pricing-col');
-});
-
-$('document').ready(function($) {
-/*----------  fixed nav
-------------------------------------------------------------------------------*/
+$('document').ready(function ($) {
+    /*----------  fixed nav
+    ------------------------------------------------------------------------------*/
     var nav = $('.main-nav');
     var pos = nav.offset().top;
     $(window).scroll(function () {
@@ -63,63 +42,90 @@ $('document').ready(function($) {
         $('body').toggleClass('fix-body', fix);
     });
 
-/*----------  smooth scroll
-------------------------------------------------------------------------------*/
+    /*----------  smooth scroll
+    ------------------------------------------------------------------------------*/
     //    jQuery.scrollSpeed(150, 900);
 
-/*----------  mobile nav
-------------------------------------------------------------------------------*/
-    $('.nav-btn').click(function(){
+    /*----------  mobile nav
+    ------------------------------------------------------------------------------*/
+    $('.nav-btn').click(function () {
         if ($('.mobile-nav').hasClass('none')) {
             $('.mobile-nav').height($('.mobile-nav').find('.header .mobile-nav').height());
             $('.mobile-nav').removeClass('none');
         } else {
-            $('.mobile-nav').height(''); 
+            $('.mobile-nav').height('');
             $('.mobile-nav').addClass('none');
         }
     });
 
-/*----------  clients
-------------------------------------------------------------------------------*/
-    $("#lista1").als({
-        visible_items: 4,
-        scrolling_items: 2,
-        orientation: "horizontal",
-        circular: "yes",
-        autoscroll: "no",
-        interval: 5000,
-        speed: 500,
-        easing: "linear",
-        direction: "right",
-        start_from: 0
-    });
+    /*----------  clients
+    ------------------------------------------------------------------------------*/
+    var $clientcarousel = $('#clients-list');
+    var clients = $clientcarousel.children().length;
+    var clientwidth = (clients * 140); // 140px width for each client item 
+    $clientcarousel.css('width', clientwidth);
 
-    //logo hover
-    $("#logo_img").hover(function()
-                         {
-        $(this).attr("src","images/als_logo_hover212x110.png");
-    },function()
-                         {
-        $(this).attr("src","images/als_logo212x110.png");
-    });
+    var rotating = true;
+    var clientspeed = 4400;
+    var seeclients = setInterval(rotateClients, clientspeed);
 
-    //logo click
-    $("#logo_img").click(function()
-                         {
-        location.href = "http://als.musings.it/index.php";
-    });
+    $(document).on({
+        mouseenter: function () {
+            rotating = false; // turn off rotation when hovering
+        },
+        mouseleave: function () {
+            rotating = true;
+        }
+    }, '.clients');
 
-    $("a[href^='http://']").attr("target","_blank");
-    $("a[href^='http://als']").attr("target","_self");
+    function rotateClients() {
+        if (rotating != false) {
+            var $first = $('#clients-list li:first');
+            $first.animate({ 'margin-left': '-140px' }, 1000, function () {
+                $first.remove().css({ 'margin-left': '0px' });
+                $('#clients-list li:last').after($first);
+            });
+        }
+    }
     
-    
+    /*----------  clients
+    ------------------------------------------------------------------------------*/
+    var $partnercarousel = $('#partners-list');
+    var partners = $partnercarousel.children().length;
+    var clientwidth = (partners * 140); // 140px width for each client item 
+    $partnercarousel.css('width', clientwidth);
+
+    var rotating = true;
+    var partnerspeed = 6400;
+    var seepartners = setInterval(rotatePartners, partnerspeed);
+
+    $(document).on({
+        mouseenter: function () {
+            rotating = false; // turn off rotation when hovering
+        },
+        mouseleave: function () {
+            rotating = true;
+        }
+    }, '.partners');
+
+    function rotatePartners() {
+        if (rotating != false) {
+            var $first = $('#partners-list li:first');
+            $first.animate({ 'margin-left': '-140px' }, 1000, function () {
+                $first.remove().css({ 'margin-left': '0px' });
+                $('#partners-list li:last').after($first);
+            });
+        }
+    }
+
+
     /*----------  validate contact form
-------------------------------------------------------------------------------*/
+    ------------------------------------------------------------------------------*/
     jQuery.validator.addMethod('answercheck', function (value, element) {
         return this.optional(element) || /^\bcat\b$/.test(value);
     }, "type the correct answer -_-");
 
-    $(function() {
+    $(function () {
         $('#contact').validate({
             rules: {
                 name: {
@@ -154,21 +160,21 @@ $('document').ready(function($) {
                     required: "sorry, wrong answer!"
                 }
             },
-            submitHandler: function(form) {
+            submitHandler: function (form) {
                 $(form).ajaxSubmit({
-                    type:"POST",
+                    type: "POST",
                     data: $(form).serialize(),
-                    url:"process.php",
-                    success: function() {
+                    url: "process.php",
+                    success: function () {
                         $('#contact :input').attr('disabled', 'disabled');
-                        $('#contact').fadeTo( "slow", 0.15, function() {
+                        $('#contact').fadeTo("slow", 0.15, function () {
                             $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
+                            $(this).find('label').css('cursor', 'default');
                             $('#success').fadeIn();
                         });
                     },
-                    error: function() {
-                        $('#contact').fadeTo( "slow", 0.15, function() {
+                    error: function () {
+                        $('#contact').fadeTo("slow", 0.15, function () {
                             $('#error').fadeIn();
                         });
                     }
@@ -198,7 +204,7 @@ $(function () {
                 step: function (now, fx) {
                     $(el).text(Math.ceil(now));
                 },
-                complete:function() {
+                complete: function () {
                     if (el.dataset.sym !== undefined) {
                         el.textContent = el.textContent.concat(el.dataset.sym)
                     }
@@ -209,7 +215,7 @@ $(function () {
 
     var reset = function reset() {
         console.log($(this).scrollTop())
-        if ($(this).scrollTop() > 120) {
+        if ($(this).scrollTop() > 83) {
             $(this).off("scroll");
             fx()
         }
@@ -220,27 +226,27 @@ $(function () {
 
 /*----------  link to tags
 ------------------------------------------------------------------------------*/
-$("ul.menubar>li>a").click(function(event){
+$("ul.menubar>li>a").click(function (event) {
     event.preventDefault();
     //calculate destination place
-    var dest=0;
-    if($(this.hash).offset().top > $(document).height()-$(window).height()){
-        dest=$(document).height()-$(window).height();
-    }else{
-        dest=$(this.hash).offset().top - 120 + "px";
+    var dest = 0;
+    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+        dest = $(document).height() - $(window).height();
+    } else {
+        dest = $(this.hash).offset().top - 83 + "px";
     }
     //go to destination
-    $('html,body').animate({scrollTop:dest}, 1000,'swing');
+    $('html,body').animate({ scrollTop: dest }, 1000, 'swing');
 });
 
 /*----------  add class to menu bar links
 ------------------------------------------------------------------------------*/
-$("ul.menubar>li").click(function(event){
+$("ul.menubar>li").click(function (event) {
     event.preventDefault();
 
     if ($(this).hasClass("is-selected")) {
         $(this).removeClass("is-selected");
-    }else{
+    } else {
         $(this).addClass("is-selected");
         $(this).siblings().removeClass('is-selected');
     }
