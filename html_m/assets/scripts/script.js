79e35f25b27a771601471249bf7c63128b902bf0
Danilo Mogin <danilo.mogin@gmail.com>
2016-07-12 23:25:13 +0200

    pre brisanja navigacije na klik

--- html_m/assets/scripts/script.js
+++ html_m/assets/scripts/script.js
@@ -30,33 +30,10 @@ equalheight = function (container) {
         }
     });
 }
-
 $('document').ready(function ($) {
-    /*----------  fixed nav
+    /*----------  equal height call
     ------------------------------------------------------------------------------*/
-    var nav = $('.main-nav');
-    var pos = nav.offset().top;
-    $(window).scroll(function () {
-        var fix = ($(this).scrollTop() > pos) ? true : false;
-        nav.toggleClass('fix-nav', fix);
-        $('body').toggleClass('fix-body', fix);
-    });
-
-    /*----------  smooth scroll
-    ------------------------------------------------------------------------------*/
-    //    jQuery.scrollSpeed(150, 900);
-
-    /*----------  mobile nav
-    ------------------------------------------------------------------------------*/
-    $('.nav-btn').click(function () {
-        if ($('.mobile-nav').hasClass('none')) {
-            $('.mobile-nav').height($('.mobile-nav').find('.header .mobile-nav').height());
-            $('.mobile-nav').removeClass('none');
-        } else {
-            $('.mobile-nav').height('');
-            $('.mobile-nav').addClass('none');
-        }
-    });
+    equalheight('.ot-box');
 
     /*----------  clients
     ------------------------------------------------------------------------------*/
@@ -69,14 +46,14 @@ $('document').ready(function ($) {
     var clientspeed = 4400;
     var seeclients = setInterval(rotateClients, clientspeed);
 
-    $(document).on({
-        mouseenter: function () {
-            rotating = false; // turn off rotation when hovering
-        },
-        mouseleave: function () {
-            rotating = true;
-        }
-    }, '.clients');
+    // $(document).on({
+    //     mouseenter: function () {
+    //         rotating = false; // turn off rotation when hovering
+    //     },
+    //     mouseleave: function () {
+    //         rotating = true;
+    //     }
+    // }, '.clients');
 
     function rotateClients() {
         if (rotating != false) {
@@ -87,8 +64,8 @@ $('document').ready(function ($) {
             });
         }
     }
-    
-    /*----------  clients
+
+    /*----------  partners
     ------------------------------------------------------------------------------*/
     var $partnercarousel = $('#partners-list');
     var partners = $partnercarousel.children().length;
@@ -96,7 +73,7 @@ $('document').ready(function ($) {
     $partnercarousel.css('width', clientwidth);
 
     var rotating = true;
-    var partnerspeed = 6400;
+    var partnerspeed = 4600;
     var seepartners = setInterval(rotatePartners, partnerspeed);
 
     $(document).on({
@@ -118,137 +95,123 @@ $('document').ready(function ($) {
         }
     }
 
-
-    /*----------  validate contact form
+    /*----------  stats numbers
     ------------------------------------------------------------------------------*/
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value);
-    }, "type the correct answer -_-");
+    var waypoint = new Waypoint({
+        element: document.getElementById("waypoint-counter"),
+        handler: function() {
+            $(".timer").countTo({
+                speed: 1000 * 8
+            });
+        },
+        offset: "-5%"
+    });
+    var waypoint = new Waypoint({
+        element: document.getElementById("waypoint-counter"),
+        handler: function() {
+            $(".timer").countTo({
+                speed: 1000 * 8
+            });
+        },
+        offset: "97%"
+    });
 
-    $(function () {
-        $('#contact').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true
-                },
-                answer: {
-                    required: true,
-                    answercheck: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                },
-                answer: {
-                    required: "sorry, wrong answer!"
-                }
-            },
-            submitHandler: function (form) {
-                $(form).ajaxSubmit({
-                    type: "POST",
-                    data: $(form).serialize(),
-                    url: "process.php",
-                    success: function () {
-                        $('#contact :input').attr('disabled', 'disabled');
-                        $('#contact').fadeTo("slow", 0.15, function () {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor', 'default');
-                            $('#success').fadeIn();
-                        });
-                    },
-                    error: function () {
-                        $('#contact').fadeTo("slow", 0.15, function () {
-                            $('#error').fadeIn();
-                        });
-                    }
-                });
+    $(document).on("scroll", onScroll);
+
+    /*----------  Back to top
+    ------------------------------------------------------------------------------*/
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 550,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 1000,
+        //grab the "back to top" link
+        $back_to_top = $(".tp-container");
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll > offset) {
+            $(".menu-btn").removeClass("show-none");
+            $(".menu-btn").addClass("show-all");
+
+            $(".main-nav").addClass("show-none");
+            $(".main-nav").removeClass("show-all");
+
+            $back_to_top.addClass("tp-show");
+        } else {
+            $(".menu-btn").addClass("show-none");
+            $(".menu-btn").removeClass("show-all");
+
+            $(".main-nav").removeClass("show-none");
+            $(".main-nav").addClass("show-all");
+
+            $back_to_top.removeClass("tp-show");
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on("click", function (event) {
+        event.preventDefault();
+        $("body,html").animate({
+            scrollTop: 0,
+        }, scroll_top_duration
+        );
+    });
+    /*----------  end of back to top  ----------*/
+
+    // burger menu
+    $(".menu-btn>img").on('click', function () {
+        for(var i = 0; i < $(".menu-btn>img").length; i++) {
+            console.log($(".menu-btn>img").length);
+            if($('.menu-btn>img').eq(i).hasClass('show-none')) {
+                $('.menu-btn>img').eq(i).removeClass('show-none');
+                $('.menu-btn>img').eq(i).addClass('show-all');
+
+                $('.mobile-nav>ul').addClass('show-all');
+                $('.mobile-nav>ul').removeClass('show-none');
+            } else {
+                $('.menu-btn>img').eq(i).addClass('show-none');
+                $('.menu-btn>img').eq(i).removeClass('show-all');
+
+                $('.mobile-nav>ul').addClass('show-none');
+                $('.mobile-nav>ul').removeClass('show-all');
             }
-        });
+        }
     });
 
 });
 
-/*----------  stats numbers
+/*----------  add class to menu bar links on scroll
 ------------------------------------------------------------------------------*/
-$(function () {
-    var fx = function fx() {
-        $(".stat-number").each(function (i, el) {
-            var data = parseInt(this.dataset.n, 10);
-            var props = {
-                "from": {
-                    "count": 0
-                },
-                "to": {
-                    "count": data
-                }
-            };
-            $(props.from).animate(props.to, {
-                duration: 3000 * 2,
-                step: function (now, fx) {
-                    $(el).text(Math.ceil(now));
-                },
-                complete: function () {
-                    if (el.dataset.sym !== undefined) {
-                        el.textContent = el.textContent.concat(el.dataset.sym)
-                    }
-                }
-            });
-        });
-    };
-
-    var reset = function reset() {
-        console.log($(this).scrollTop())
-        if ($(this).scrollTop() > 83) {
-            $(this).off("scroll");
-            fx()
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $("ul.nav-script>li>a").each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $("ul.nav-script>li").removeClass("active");
+            currLink.addClass("active");
         }
-    };
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
 
-    $(window).on("scroll", reset);
-});
-
-/*----------  link to tags
+/*----------  scroll to position
 ------------------------------------------------------------------------------*/
-$("ul.menubar>li>a").click(function (event) {
-    event.preventDefault();
-    //calculate destination place
-    var dest = 0;
-    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-        dest = $(document).height() - $(window).height();
-    } else {
-        dest = $(this.hash).offset().top - 83 + "px";
-    }
-    //go to destination
-    $('html,body').animate({ scrollTop: dest }, 1000, 'swing');
+$("ul.nav-script>li>a, .mobile-nav>ul>li>a").on("click", function () {
+    var scrollAnchor = $(this).attr("data-scroll"),
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top;
+    $("body,html").animate({
+        scrollTop: scrollPoint
+    }, 1000);
+
+    return false;
+})
+/*----------  end of scrol to position  ----------*/
+
+/*----------  reload page
+    ------------------------------------------------------------------------------*/
+$("#reload").click(function () {
+    location.reload();
 });
-
-/*----------  add class to menu bar links
-------------------------------------------------------------------------------*/
-$("ul.menubar>li").click(function (event) {
-    event.preventDefault();
-
-    if ($(this).hasClass("is-selected")) {
-        $(this).removeClass("is-selected");
-    } else {
-        $(this).addClass("is-selected");
-        $(this).siblings().removeClass('is-selected');
-    }
-});
-
